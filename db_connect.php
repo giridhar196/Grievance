3e9d4e793cb4ae8bfab54242f448de842ac04e66
Giridhar Addagalla <giridhar196@gmail.com>
2019-02-01 23:12:12 +0530

    Home page and back button are added

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,8 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "grievance";
-
 // create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
-
 // check connection 
 if($connect->connect_error) {
 	die("Connection Failed : " . $connect->connect_error);
